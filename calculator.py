533f3cb84a7e9bd62c2bb6e6e94a0b811f0ac76e
srushtipatil123 <srushti.p@bigdoorsolutions.co.in>
2024-08-03 10:50:40 +0530

    added the calcultor.py file

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def addition(number1,number2):
+    result = number1 + number2
+    return result
+def subtraction(num1,num2):
+    res = num1 - num2
+    return res
+def multipication(num1,num2):
+    res = num1 * num2
+    return res
+def division(num1,num2):
+    res = num1 / num2
+    return res
+div = division(10,2)
+print(f"The division of two numbers is: {div}")
+mul = multipication(10,5)
+print(f"The multication of two numbers is: {mul}")
+sub = subtraction(10,5)
+print(f"The subtraction of two numbers is: {sub}")
+sum = addition(10,20)
+print(f"The sum of two numbers is: {sum}")
